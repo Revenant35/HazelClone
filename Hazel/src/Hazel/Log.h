6bf1b5280e36d2abad9a9cf27de200efa33c8b23
Zach Brown <zbrown@herzog.com>
2024-06-21 10:47:43 -0500

    styling changes

--- Hazel/src/Hazel/Log.h
+++ Hazel/src/Hazel/Log.h
@@ -12,9 +12,8 @@ namespace Hazel
 
         static void Init();
 
-        static std::shared_ptr<spdlog::logger> & GetCoreLogger();
-        static std::shared_ptr<spdlog::logger> & GetClientLogger();
-
+        static std::shared_ptr<spdlog::logger> & GetCoreLogger() { return s_CoreLogger; }
+        static std::shared_ptr<spdlog::logger> & GetClientLogger() { return s_ClientLogger; }
     private:
 
         static std::shared_ptr<spdlog::logger> s_CoreLogger;
