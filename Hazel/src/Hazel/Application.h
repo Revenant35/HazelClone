a2928ad1f1c7ebcaf99c73a400341c3e9c51665c
Zach Brown <zbrown@herzog.com>
2024-06-24 08:56:19 -0500

    Adding ImGui layer

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -22,12 +22,18 @@ namespace Hazel {
 
         void PushLayer(Layer* layer);
         void PushOverlay(Layer* layer);
+        
+        Window& GetWindow() const { return *m_Window; }
+
+        static Application& Get() { return *s_Instance; }
     private:
         bool OnWindowClose(WindowCloseEvent& e);
 
         std::unique_ptr<Window> m_Window;
         bool m_Running = true;
         LayerStack m_LayerStack;
+        
+        static Application* s_Instance;
     };
 
     // To be defined in CLIENT
